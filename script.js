8e4e15f8b1bb8c89f6312e6c8527d6ee27aeb01d
190031754-vamsi <190031754cse@gmail.com>
2024-08-27 16:48:20 +0530

    ReCaptcha Validation Added

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function beforesubmit(){
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log("inputdate.value", inputdate.value);  //the value getting here is String, convert into Date type. Before conversion it into Date data type. Find its Local of the logged user(System.debug(UserInfo.getLocale()))
+let capthachecked = false;
 
-    let formmattedDate = new Date(inputdate.value).toLocaleDateString("en-IN"); //Remove underscore , and add hyphen
-    // If you want to formate the date with Logged user location, then add the method of 'toLocalDareString()'
-    outputdate.value = formmattedDate;
 
+function submitbuttonclicks(){
+   if(capthachecked){
+      let outputdate = document.querySelector(".outputdate");
+      let inputdate = document.querySelector(".inputdate");
+      console.log("inputdate.value", inputdate.value);  //the value getting here is String, convert into Date type. Before conversion it into Date data type. Find its Local of the logged user(System.debug(UserInfo.getLocale()))
+  
+      let formmattedDate = new Date(inputdate.value).toLocaleDateString("en-IN"); //Remove underscore , and add hyphen
+      // If you want to formate the date with Logged user location, then add the method of 'toLocalDareString()'
+      outputdate.value = formmattedDate;  
+   }
+   else{
+      alert('Please check the reCaptcha box to submit the lead');
+      event.preventDefault(); // This method will stop the submission of the form.
+   }
 }
 
 function timestamp()
@@ -20,3 +28,8 @@ function timestamp()
    } 
 } 
 setInterval(timestamp, 500); 
+
+
+function capthchasucess(){
+   capthachecked = true;
+}
